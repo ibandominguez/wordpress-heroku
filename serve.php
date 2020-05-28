5147f6c53c8b8d7c86add19979a1948ec7b78b1d
ibandominguez <domingueznoda@gmail.com>
2020-05-28 12:10:41 +0100

    fix serve script

--- serve.php
+++ serve.php
@@ -1,12 +1,25 @@
 <?php
 
+function rmdir_recursive($directory) {
+  foreach (scandir($directory) as $file) {
+    if ('.' === $file || '..' === $file) continue;
+    if (is_dir("$directory/$file")) rmdir_recursive("$directory/$file");
+    else unlink("$directory/$file");
+  }
+  rmdir($directory);
+}
+
 foreach (array(
   __DIR__.'/config/wp-config.php' => __DIR__.'/public/wp-config.php',
   __DIR__.'/plugins' => __DIR__.'/public/wp-content/plugins',
-  __DIR__.'/theme' => __DIR__.'/public/wp-content/themes/theme'
+  __DIR__.'/themes' => __DIR__.'/public/wp-content/themes'
 ) as $key => $value):
+  if (is_link($value)):
+    continue;
+  endif;
+
   if (file_exists($value) && is_dir($value)):
-    @rmdir($value);
+    @rmdir_recursive($value);
   elseif (file_exists($value) && is_file($value)):
     @unlink($value);
   endif;
