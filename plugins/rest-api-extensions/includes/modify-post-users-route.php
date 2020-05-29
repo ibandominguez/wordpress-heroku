32895fac5f6bbc9d17f982c514394e48352d352b
ibandominguez <domingueznoda@gmail.com>
2020-05-29 07:16:28 +0100

    add basic auth token 2 request

--- plugins/rest-api-extensions/includes/modify-post-users-route.php
+++ plugins/rest-api-extensions/includes/modify-post-users-route.php
@@ -6,8 +6,18 @@ function modifyPostUsersRoute() {
   register_rest_route('wp/v2', '/users', array(
     array(
       'methods'             => WP_REST_Server::CREATABLE,
-      'callback'            => array($users_controller, 'create_item'),
       'args'                => $users_controller->get_endpoint_args_for_item_schema(WP_REST_Server::CREATABLE),
+      'callback'            => function($request) use ($users_controller) {
+        $response = $users_controller->create_item($request);
+
+        if (!is_wp_error($response)):
+          $responseData = $response->get_data();
+          $responseData['basic_authorization_header'] = signBasicHeaderToken($request['email'], $request['password']);
+          $response->set_data($responseData);
+        endif;
+        
+        return $response;
+      },
       'permission_callback' => function($request) {
         if (!current_user_can('create_users') && ($request['roles'] && $request['roles'] !== array('subscriber'))):
           return new WP_Error(
