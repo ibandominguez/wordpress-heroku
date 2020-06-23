95432093ad667d5322a7182d395078840f62a3b8
ibandominguez <domingueznoda@gmail.com>
2020-06-23 14:57:09 +0100

    initial themes getting confused with theme wp repo

--- themes/default/index.php
+++ themes/default/index.php
@@ -1,12 +1,21 @@
-<?php
+<!doctype html>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+    <?php wp_head(); ?>
+  </head>
+  <body>
+    <?php if (have_posts()): while (have_posts()): the_post(); ?>
+      <?php the_title(); ?>
+      <?php the_content(); ?>
+    <?php endwhile; endif;?>
 
-global $wp_query;
-global $wpdb;
-
-header('Content-Type: application/json');
-
-die(json_encode([
-  'env' => getenv(),
-  'global $wp_query' => $wp_query,
-  'global $wpdb' => $wpdb
-], JSON_PRETTY_PRINT));
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <?php wp_footer(); ?>
+  </body>
+</html>
