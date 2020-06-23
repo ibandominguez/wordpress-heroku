87a1ddcd6266ef750c911407565714555da7e242
ibandominguez <domingueznoda@gmail.com>
2020-06-23 12:28:42 +0100

    default dump index page

--- themes/carreras/index.php
+++ themes/carreras/index.php
@@ -6,6 +6,7 @@ global $wpdb;
 header('Content-Type: application/json');
 
 die(json_encode([
+  'DISALLOW_FILE_EDIT' => DISALLOW_FILE_EDIT,
   'global $wp_query' => $wp_query,
   'global $wpdb' => $wpdb
 ], JSON_PRETTY_PRINT));
--- themes/default/index.php
+++ themes/default/index.php
@@ -1,15 +1,12 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <title><?php wp_title(); ?></title>
-  </head>
-  <body>
-    <div class="container py-5">
-      <h2>INDEX.php</h2>
-      <p>Time to create your theme</p>
-    </div>
-  </body>
-</html>
+<?php
+
+global $wp_query;
+global $wpdb;
+
+header('Content-Type: application/json');
+
+die(json_encode([
+  'env' => getenv(),
+  'global $wp_query' => $wp_query,
+  'global $wpdb' => $wpdb
+], JSON_PRETTY_PRINT));
