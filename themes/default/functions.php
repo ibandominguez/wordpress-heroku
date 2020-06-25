38a1f4c81f12300d30a81b634ebcca7c97ab5ac1
ibandominguez <domingueznoda@gmail.com>
2020-06-25 09:54:29 +0100

    Default theme base

--- themes/default/functions.php
+++ themes/default/functions.php
@@ -1,1 +1,29 @@
 <?php
+
+require_once __DIR__.'/includes/navwalker.php';
+
+add_action('init', function() {
+  register_nav_menus([
+    'menu' => __('Menu')
+  ]);
+});
+
+add_action('wp_enqueue_scripts', function() {
+  wp_enqueue_style('bootstrap-css', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css', false, null, 'all');
+  wp_enqueue_script('popper-js', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js', ['jquery'], null, true);
+  wp_enqueue_script('bootstrap-js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js', ['jquery'], null, true);
+});
+
+add_action('after_setup_theme', function() {
+  add_theme_support('custom-logo', [
+    'height'      => 100,
+    'width'       => 400,
+    'flex-height' => true,
+    'flex-width'  => true,
+    'header-text' => ['site-title', 'site-description']
+  ]);
+});
+
+add_action('admin_menu', function() {
+  // TODO: Add theme options
+});
--- themes/default/index.php
+++ themes/default/index.php
@@ -1,21 +1,38 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
   <head>
-    <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
     <?php wp_head(); ?>
   </head>
   <body>
-    <?php if (have_posts()): while (have_posts()): the_post(); ?>
-      <?php the_title(); ?>
-      <?php the_content(); ?>
-    <?php endwhile; endif;?>
+    <header class="d-flex align-items-center">
+      <?php if (function_exists('the_custom_logo') && has_custom_logo()): ?>
+        <?php the_custom_logo(); ?>
+      <?php else: ?>
+        <h2 class="p-2">
+          <a href="<?php echo esc_url(home_url('/')); ?>" rel="home" title="<?php bloginfo('name'); ?>">
+            <?php bloginfo( 'name' ); ?>
+          </a>
+        </h2>
+      <?php endif; ?>
 
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+      <?php wp_nav_menu([
+        'menu' => 'menu',
+        'depth' => 2,
+        'container' => 'nav',
+        'menu_class' => 'nav',
+        'walker' => new wp_bootstrap_navwalker()
+      ]); ?>
+    </header>
+
+    <?php if (have_posts()): while (have_posts()): the_post(); ?>
+      <?php the_content(); ?>
+    <?php endwhile; else: ?>
+      <!-- TODO: Translated 404 -->
+    <?php endif; ?>
+
     <?php wp_footer(); ?>
   </body>
 </html>
