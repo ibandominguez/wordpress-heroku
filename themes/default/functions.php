d93aea1a11f6ad670a07a39a2d233a7f438bca66
ibandominguez <domingueznoda@gmail.com>
2020-06-26 13:40:08 +0100

    default theme desc

--- themes/default/functions.php
+++ themes/default/functions.php
@@ -27,3 +27,14 @@ add_action('after_setup_theme', function() {
 add_action('admin_menu', function() {
   // TODO: Add theme options
 });
+
+add_action('wp_dashboard_setup', function() {
+  // TODO: Custom welcome panel full width
+  // remove_action('welcome_panel', 'wp_welcome_panel');
+  // add_action('welcome_panel', <MY_CALLABLE>);
+
+  wp_add_dashboard_widget('theme_support', 'Theme Support', function() { ?>
+    <h4>Theme support</h4>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <?php });
+});
