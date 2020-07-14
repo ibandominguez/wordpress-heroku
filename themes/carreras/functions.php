1160e291eb238a4d545b0c07fd6f9c20694574ae
Ibán Domínguez Noda <domingueznoda@gmail.com>
2020-07-14 09:52:56 +0800

    custom logo

--- themes/carreras/functions.php
+++ themes/carreras/functions.php
@@ -435,3 +435,27 @@ add_action('rest_api_init', function() {
     }
   ));
 });
+
+add_action('after_setup_theme', function() {
+  add_theme_support('custom-logo');
+});
+
+add_action('login_enqueue_scripts', function() {
+  if (has_custom_logo()):
+    $logoId = get_theme_mod('custom_logo');
+    $logoData = wp_get_attachment_image_src($logoId, 'full');
+
+    print("
+      <script>
+      window.onload = function() {
+        document.getElementById('backtoblog').remove();
+        document.getElementById('nav').remove();
+        document.querySelector('body.login div#login h1 a').href = window.location.href;
+      }
+      </script>
+      <style>
+      body.login div#login h1 a { background-image: url({$logoData[0]}); }
+      </style>
+    ");
+  endif;
+});
