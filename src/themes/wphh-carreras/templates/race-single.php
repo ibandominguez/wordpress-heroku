5fea0cc5c6ac13c13bddd74425063ae33af7555e
Ibán Domínguez Noda <domingueznoda@gmail.com>
2020-11-18 20:57:56 +0000

    added head for plugins to be used

--- src/themes/wphh-carreras/templates/race-single.php
+++ src/themes/wphh-carreras/templates/race-single.php
@@ -23,9 +23,10 @@ endif;
 <html <?php language_attributes(); ?>>
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <?php wp_head(); ?>
   <script src="https://js.stripe.com/v3/"></script>
 </head>
 <body class="bg-light d-flex align-items-center justify-content-center">
