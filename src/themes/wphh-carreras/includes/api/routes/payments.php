77f2abc8fd3bb246efe4937605076c7bb8eea723
Ibán Domínguez Noda <domingueznoda@gmail.com>
2020-11-18 18:21:21 +0000

    jsonParams test

--- src/themes/wphh-carreras/includes/api/routes/payments.php
+++ src/themes/wphh-carreras/includes/api/routes/payments.php
@@ -154,9 +154,23 @@ register_rest_route('wp/v2', '/webhooks', [
     return true;
   },
   'callback' => function (WP_REST_Request $request) {
-    return new WP_REST_Response(
-      $request->get_json_params(),
-      $response['response']['code']
-    );
+    $body = $request->get_json_params();
+
+    if (
+      $body['type'] === 'checkout.session.completed' &&
+      !empty($body['data']['object']['client_reference_id']) &&
+      !empty($body['data']['object']['client_reference_id'])
+    ):
+      $meta = json_decode($body['data']['object']['client_reference_id']);
+      add_user_meta($meta->user_id, 'race_payments', $meta->race_id);
+      return new WP_REST_Response(
+        'message' => "Added user:{$meta->user_id} to race:{$meta->race_id}",
+        201
+      );
+    endif;
+
+    return new WP_REST_Response([
+      'message' => "Received event $body['type']"
+    ], 200);
   }
 ]);
