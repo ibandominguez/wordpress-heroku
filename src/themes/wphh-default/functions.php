4ac8d76be8dd27fa29984b28dc52e1e8965b40c4
Ibán Domínguez Noda <domingueznoda@gmail.com>
2021-03-02 00:06:51 +0000

    theme support and configs

--- src/themes/wphh-default/functions.php
+++ src/themes/wphh-default/functions.php
@@ -2,11 +2,6 @@
 
 require_once __DIR__.'/includes/Bootstrap_NavWalker.php';
 
-add_action('init', function () {
-  add_theme_support('wp-block-styles');
-  add_theme_support('align-wide');
-});
-
 add_action('wp_enqueue_scripts', function() {
   wp_enqueue_style('bootstrap-css', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css', false, null, 'all');
   wp_enqueue_style('style', get_stylesheet_uri());
@@ -16,20 +11,21 @@ add_action('wp_enqueue_scripts', function() {
 
 add_action('after_setup_theme', function() {
   register_nav_menu('primary', __('Primary Menu', 'wphh-default'));
-  add_theme_support('custom-logo', ['height' => 75, 'width' => 75]);
-});
-
-add_action('admin_menu', function() {
-  // TODO: Add theme options
+  add_theme_support('custom-logo', []);
+  add_theme_support('wp-block-styles');
+  add_theme_support('align-wide');
 });
 
 add_action('wp_dashboard_setup', function() {
-  // TODO: Custom welcome panel full width
-  // remove_action('welcome_panel', 'wp_welcome_panel');
-  // add_action('welcome_panel', <callable>);
+  /* TODO: Custom welcome panel full width
+  remove_action('welcome_panel', 'wp_welcome_panel');
+  add_action('welcome_panel', <callable>);
+  */
 
+  /* TODO: Create theme widget
   wp_add_dashboard_widget('theme_support', 'Theme Support', function() { ?>
     <h4>Theme support</h4>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   <?php });
+  */
 });
